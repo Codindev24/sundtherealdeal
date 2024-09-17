9e0cda34747ec7ddf1a997c168d584b4c07be41d
Codindev24 <odinnmoddar@gmail.com>
2024-09-17 12:11:31 +0000

    Front Page

--- src/pages/Heim.jsx
+++ src/pages/Heim.jsx
@@ -1,5 +1,6 @@
 import Laga from "../bits/Laga";
 import Tilk from "../bits/Tilk";
+import { Parallax } from 'react-parallax';
 import '../sass/heim.scss';
 
 
@@ -9,13 +10,72 @@ function App() {
     <>
       <div className="app">
 
-      <div className="lagatilk flex justify-center">
+      <Parallax blur={2} bgImage="https://res.cloudinary.com/itb-database/image/upload/s--bMF7aDwt--/c_fill,dpr_auto,f_auto,q_auto:eco,w_1470/v1/ServiceProviders/f898d328c7f7c459733d48cc2d83f1ad" bgImageAlt="the cat" strength={200}>
         
+      <h1 className="lagatilk flex justify-evenly">
+
       <Laga />
 
       <Tilk />
 
-      </div>
+      </h1>{/* .lagatilk */}
+
+      </Parallax>
+
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minus odio libero, delectus vel commodi natus exercitationem? Velit praesentium sunt ipsa nisi architecto obcaecati exercitationem eos, et iste! Ducimus, voluptatem?
 
       </div>
        
