7df3c6964441d0385837ef320cc0f9d804fd040b
Codindev24 <odinnmoddar@gmail.com>
2024-09-20 14:55:12 +0000

    Vinna í SKólakort

--- src/pages/Skolakort.jsx
+++ src/pages/Skolakort.jsx
@@ -26,12 +26,68 @@ function Skolakort() {
     <>
         <div className="skolakort">
 
+          <div className="newskortbar">
+
+          <div className="breadcrumbs text-sm">
+          <ul>
+            <li>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-4 w-4 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                </svg>
+                Heim
+              </a>
+            </li>
+            <li>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-4 w-4 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                </svg>
+                Skólakort
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-4 w-4 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                </svg>
+                Nýtt Skólakort
+              </a>
+            </li>
+          </ul>
+        </div>
+
+          </div>{/* .newskortbar */}
+
          {/* <h1 className="skort flex justify-center">Skólakort</h1>  */}
 
          <div className="search flex justify-center">
 
-
-         <input type="text" value={searchItem} onChange={handleInputChange} placeholder='Type to search' />
+         <input type="text" value={searchItem} onChange={handleInputChange} placeholder='Leita að korti..' />
 
          </div>{/* .search */}
 
